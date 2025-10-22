9d52db928f9193ead6f71d9fe4d0f9b39b63ddea
Lephung89 <162795293+Lephung89@users.noreply.github.com>
2025-10-22 10:51:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, Te
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import ConversationalRetrievalChain
 import requests
 import json
 from langchain.memory import ConversationBufferWindowMemory
